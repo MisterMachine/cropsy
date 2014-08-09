8119460aab09635579f41a368ee51cedf403c03f
slowbot <jesse@mistermachine.com>
2014-08-09 01:35:28 -0700

    Adding cropbox and hammer for dimensions and touch

--- app/scripts/views/application_view.js
+++ app/scripts/views/application_view.js
@@ -1,7 +1,12 @@
 Cropsy.ApplicationView = Ember.View.extend({
 
   didInsertElement: function() {
-    $(".viewer").zoomer();
+    $('.viewer').zoomer();
+
+    $( '.crop' ).cropbox({width: 500, height: 500})
+    .on('cropbox', function (e, result) {
+        console.log(result);
+    });
   }
-  
+
 });
