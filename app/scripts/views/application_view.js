0d7a14959c02c564e9bc22b2a220ac666510c256
menslow <michael.enslow@gmail.com>
2014-08-08 18:50:49 -0700

    added windwo resize event for zoomer

--- app/scripts/views/application_view.js
+++ app/scripts/views/application_view.js
@@ -2,6 +2,11 @@ Cropsy.ApplicationView = Ember.View.extend({
 
   didInsertElement: function() {
     $(".viewer").zoomer();
+
+    $(window).on("resize", function(e) {
+      $(".viewer").zoomer("resize");
+    });
+
   }
 
 });
