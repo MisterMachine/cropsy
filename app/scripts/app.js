9d541aa2acf257957eea3897ec374056d585658b
menslow <michael.enslow@gmail.com>
2014-08-08 18:32:41 -0700

    added basic zoomer plugin test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,6 @@ var Cropsy = window.Cropsy = Ember.Application.create({
 
 Cropsy.applicationName = "Cropsy";
 
-
 /* Order and include as you please. */
 require('scripts/controllers/*');
 require('scripts/store');
