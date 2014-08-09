8db6a514ad90c3e2ac158e66c300823891d0ff25
slowbot <jesse@mistermachine.com>
2014-08-09 16:08:36 -0700

    Cleaning up component styles and adding wrapper to toolbar

--- app/scripts/views/application_view.js
+++ app/scripts/views/application_view.js
@@ -3,10 +3,10 @@ Cropsy.ApplicationView = Ember.View.extend({
   didInsertElement: function() {
     $('.viewer').zoomer();
 
-    $( '.crop' ).cropbox({width: 500, height: 500})
+    $('.crop').cropbox({width: 600, height: 250})
     .on('cropbox', function (e, result) {
         console.log(result);
     });
-  }
+  }  
 
 });
