cecdb5a4af76be999164e913380557fb7ae9e194
slowbot <jesse@mistermachine.com>
2014-08-09 17:07:03 -0700

    Adding cropper plugin and initial tests

--- app/scripts/views/application_view.js
+++ app/scripts/views/application_view.js
@@ -7,6 +7,40 @@ Cropsy.ApplicationView = Ember.View.extend({
     .on('cropbox', function (e, result) {
         console.log(result);
     });
+
+    var $image = $(".img-container img"),
+    $dataX = $("#data-x"),
+    $dataY = $("#data-y"),
+    $dataHeight = $("#data-height"),
+    $dataWidth = $("#data-width");
+
+    $image.cropper({
+        aspectRatio: 16 / 9,
+        data: {
+            x: 480,
+            y: 60,
+            width: 640,
+            height: 360
+        },
+        preview: ".img-preview",
+        done: function(data) {
+            $dataX.val(data.x);
+            $dataY.val(data.y);
+            $dataHeight.val(data.height);
+            $dataWidth.val(data.width);
+        }
+    });
+
+
+    $("#reset").click(function() {
+        $image.cropper("reset");
+    });
+
+
+    $("#free-ratio").click(function() {
+        $image.cropper("setAspectRatio", "auto");
+    });
+
   }  
 
 });
