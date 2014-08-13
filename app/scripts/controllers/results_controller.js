4c6eb60a5f3b51e88b08f9577c442eb180de962b
menslow <michael.enslow@gmail.com>
2014-08-12 17:25:22 -0700

    cleaned up unused actions

--- app/scripts/controllers/results_controller.js
+++ app/scripts/controllers/results_controller.js
@@ -1,11 +1,5 @@
 Cropsy.ResultsController = Ember.ObjectController.extend({
 
-  // computed properties
-  cropX: 50,
-  cropY: 50,
-  cropW: 400,
-  cropH: 400,
-
   actions: {
 
     cancel: function(image) {
